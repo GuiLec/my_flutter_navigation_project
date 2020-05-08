ca1eef0dba65533dc46d7193d46b56389c20ca2a
GuiLec <glecallier@yahoo.com>
2020-05-08 15:56:39 +0200

    feat: 🎸 implement navigation between home to family

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_navigation_project/pages/Family.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, new MaterialPageRoute(builder: (context) => Family()));
+        },
+      ),
       body: Container(
         child: Center(
           child: Text('Home'),
