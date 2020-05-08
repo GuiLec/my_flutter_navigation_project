029154c8dffe6aa26eac07d7561d911f96a6f876
GuiLec <glecallier@yahoo.com>
2020-05-08 15:59:56 +0200

    feat: 🎸 add Home title

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -5,6 +5,9 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('La famille Bonnet'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
