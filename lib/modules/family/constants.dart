777207960c374ba89cc58515690809cfb460e9b5
GuiLec <glecallier@yahoo.com>
2020-05-08 23:23:04 +0200

    refactor: 💡 extract family on family module

---
+++ lib/modules/family/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_navigation_project/modules/family/interface.dart';
+
+List<FamilyData> family = [
+  FamilyData('Mamoune', AssetImage('lib/assets/images/mamoune.png')),
+  FamilyData(
+      'Grands Bonnet ("Yéyé")', AssetImage('lib/assets/images/gd_bonnet.png')),
+  FamilyData('Lecallier', AssetImage('lib/assets/images/lecallier.jpeg')),
+  FamilyData('Hélène', AssetImage('lib/assets/images/helene.jpg')),
+  FamilyData('Réquillart', AssetImage('lib/assets/images/requillart.png')),
+  FamilyData(
+      'Petits Bonnet ("Riri")', AssetImage('lib/assets/images/pti_bonnet.png')),
+];
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_navigation_project/components/FamilyItem.dart';
-import 'package:my_flutter_navigation_project/modules/family/interface.dart';
-
-List<FamilyData> family = [
-  FamilyData('Mamoune', AssetImage('lib/assets/images/mamoune.png')),
-  FamilyData(
-      'Grands Bonnet ("Yéyé")', AssetImage('lib/assets/images/gd_bonnet.png')),
-  FamilyData('Lecallier', AssetImage('lib/assets/images/lecallier.jpeg')),
-  FamilyData('Hélène', AssetImage('lib/assets/images/helene.jpg')),
-  FamilyData('Réquillart', AssetImage('lib/assets/images/requillart.png')),
-  FamilyData(
-      'Petits Bonnet ("Riri")', AssetImage('lib/assets/images/pti_bonnet.png')),
-];
+import 'package:my_flutter_navigation_project/modules/family/constants.dart';
 
 class Home extends StatelessWidget {
   @override
