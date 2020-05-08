2b51f6026f1731487d26fe98c2ec5193b4dffa89
GuiLec <glecallier@yahoo.com>
2020-05-08 16:00:52 +0200

    feat: 🎸 add appbar for family page

--- lib/pages/Family.dart
+++ lib/pages/Family.dart
@@ -4,6 +4,9 @@ class Family extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Famille Lecallier'),
+      ),
       body: Container(
         child: Center(
           child: Text('Family'),
