c6ec1925a149aa24a34781de6b3a1a2f7cecf399
GuiLec <glecallier@yahoo.com>
2020-05-08 22:08:07 +0200

    feat: 🎸 add Helene and Mamoune to Home page

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -10,9 +10,11 @@ class FamilyData {
 }
 
 List<FamilyData> family = [
+  FamilyData('Mamoune', AssetImage('lib/assets/images/mamoune.png')),
   FamilyData(
       'Grands Bonnet ("Yéyé")', AssetImage('lib/assets/images/gd_bonnet.png')),
   FamilyData('Lecallier', AssetImage('lib/assets/images/lecallier.jpeg')),
+  FamilyData('Hélène', AssetImage('lib/assets/images/helene.jpg')),
   FamilyData('Réquillart', AssetImage('lib/assets/images/requillart.png')),
   FamilyData(
       'Petits Bonnet ("Riri")', AssetImage('lib/assets/images/pti_bonnet.png')),
