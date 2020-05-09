941069be45c9543c710a17791307b2adcca223f1
GuiLec <glecallier@yahoo.com>
2020-05-09 23:30:16 +0200

    feat: 🎸 small style improvement

--- lib/components/FamilyItem.dart
+++ lib/components/FamilyItem.dart
@@ -16,50 +16,59 @@ class FamilyItem extends StatelessWidget {
         Navigator.push(context,
             new MaterialPageRoute(builder: (context) => Family(familyData)));
       },
-      child: Row(
-        children: <Widget>[
-          Container(
-            color: Colors.grey[200],
-            child: Image(
-              height: 100,
-              width: 130,
-              image: familyData.familyImage,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.cyan[200],
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: Image(
+                fit: BoxFit.fill,
+                height: 100,
+                width: 130,
+                image: familyData.familyImage,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child: Text(
-                    familyData.name,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      familyData.name,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[900]),
+                    ),
                   ),
-                ),
-                Row(
-                  children: familyData.familyMembers
-                      .map(
-                        (member) => Miniature(
-                          member.memberImage,
-                          isSmall:
-                              member.generation == Generation.GRAND_CHILDREN,
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                new MaterialPageRoute(
-                                    builder: (context) =>
-                                        FamilyMemberPage(member)));
-                          },
-                        ),
-                      )
-                      .toList(),
-                )
-              ],
+                  Row(
+                    children: familyData.familyMembers
+                        .map(
+                          (member) => Miniature(
+                            member.memberImage,
+                            isSmall:
+                                member.generation == Generation.GRAND_CHILDREN,
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) =>
+                                          FamilyMemberPage(member)));
+                            },
+                          ),
+                        )
+                        .toList(),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
